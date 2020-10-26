07aea1a58adff7e9bf2584d5f438b977e2174620
ClaytonCampbell <ccampbell9764@gmail.com>
2020-10-26 14:33:52 -0400

    character

---
+++ DreamAPI.Data/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamAPI.Data
+{
+    public class Character
+    {
+        [Key]
+        public int CharacterId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Relationship { get; set; }
+    }
+}
