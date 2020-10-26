b9f39ee120945b835d2bf0197c9719e3eac1194e
Jamie Williams <jlatwil@gmail.com>
2020-10-26 11:56:19 -0400

    added properties. changed property name to resolve error.

--- DreamAPI.Data/Emotion.cs
+++ DreamAPI.Data/Emotion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace DreamAPI.Data
 {
     public class Emotion
     {
-
+        [Key]
+        public int EmotionId { get; set; }
+        [Required]
+        public string EmotionType { get; set; }
+        [Required]
+        public int DreamId { get; set; }
     }
 }
