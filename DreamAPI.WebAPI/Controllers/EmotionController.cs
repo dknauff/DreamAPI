8589569b8e0ec7bee2aad9a18a6e5f587045f971
Jamie Williams <jlatwil@gmail.com>
2020-10-26 14:12:25 -0400

    completed method

--- DreamAPI.WebAPI/Controllers/EmotionController.cs
+++ DreamAPI.WebAPI/Controllers/EmotionController.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using DreamAPI.Services;
+using Microsoft.AspNet.Identity;
 
 namespace DreamAPI.WebAPI.Controllers
 {
-    public class EmotionController : ApiController
+    [Authorize]
+    public class NoteController : ApiController
     {
-
+        private EmotionService CreateNoteService()
+        {
+            var emotionId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new EmotionService(emotionId);
+            return noteService;
+        }
     }
 }
